44ee5a26993b5ef2deb9ac762fdce930d5de45ba
luoyongjian <7674555+luoyongjian@user.noreply.gitee.com>
2024-06-05 18:15:00 +0800

    更新入口文件，更新配置项

--- output/bundle.js
+++
@@ -1,7 +0,0 @@
-var version = "1.0.0";
-
-function c() {
-    console.log(`当前版本号%c${version}`, 'color: red;');
-}
-
-export { c as default };
--- src/main.js
+++ src/main.js
@@ -1,5 +1,53 @@
+// 引入所需
 import { version } from '../package.json';
 
-export default function c() {
-    console.log(`当前版本号%c${version}`, 'color: red;');
+// 定义类型
+const TYPE_DESC = [
+    { '字符串': '%s' },
+    { '整数': '%d or %i' },
+    { '浮点数': '%f' },
+    { '对象': '%o' },
+    { 'CSS 样式': '%c' }
+]
+const typeColor = (type = "default") => {
+    let color = "";
+    switch (type) {
+        case "default":
+            color = "#35495E";
+            break;
+        case "primary":
+            color = "#3488ff";
+            break;
+        case "success":
+            color = "#43B883";
+            break;
+        case "warning":
+            color = "#e6a23c";
+            break;
+        case "danger":
+            color = "#f56c6c";
+            break;
+        default:
+            break;
+    }
+    return color;
+};
+
+// 定义对象
+let v_aot = {}
+
+// 定义方法
+v_aot.txt = (label, value, type = "default") => {
+    console.log(`${label}%c${value}`, `color: ${typeColor(type)};`);
 }
+v_aot.capsule = (title, info, type = "default") => {
+    console.log(
+        `%c ${title} %c ${info} %c`,
+        "background:#35495E; padding: 1px; border-radius: 3px 0 0 3px; color: #fff;",
+        `background:${typeColor(type)}; padding: 1px; border-radius: 0 3px 3px 0;  color: #fff;border-left: 1px solid #fff;`,
+        "background:transparent"
+    );
+};
+
+
+export default v_aot
